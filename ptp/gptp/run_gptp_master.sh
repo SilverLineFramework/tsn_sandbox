cc8f083e82cd17c0940e295a0de2539fe5d11ebd
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-12-16 21:25:44 +0000

    switch gptp interface for new nucs

--- ptp/gptp/run_gptp_master.sh
+++ ptp/gptp/run_gptp_master.sh
@@ -7,11 +7,11 @@ if [[ `whoami` != "root" ]]; then
 fi
 
 echo "Start ptp..."
-sudo ptp4l -f gPTP_master.cfg -i enp87s0 -m &> ptp.log &
+sudo ptp4l -f gPTP_master.cfg -i enp88s0 -m &> ptp.log &
 echo "ptp4l is running in bg as pid $!"
 
 sleep 2
 
 echo "Start phc2sys..."
-sudo phc2sys -c enp87s0 -s CLOCK_REALTIME -m -O 37 &> phc2sys.log &
+sudo phc2sys -c enp88s0 -s CLOCK_REALTIME -m -O 37 &> phc2sys.log &
 echo "phc2sys (sync NIC clock to sys clock) is running in bg as pid $!"
