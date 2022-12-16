f5a32582fd5c598c8fdabaed9123975ebb828368
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-12-16 22:04:49 +0000

    test just ptp4l

--- ptp/gptp/run_gptp_master.sh
+++ ptp/gptp/run_gptp_master.sh
@@ -12,6 +12,6 @@ echo "ptp4l is running in bg as pid $!"
 
 sleep 2
 
-echo "Start phc2sys..."
-sudo phc2sys -c enp88s0 -s CLOCK_REALTIME -m -O 37 &> phc2sys.log &
-echo "phc2sys (sync NIC clock to sys clock) is running in bg as pid $!"
+#echo "Start phc2sys..."
+#sudo phc2sys -c enp88s0 -s CLOCK_REALTIME -m -O 37 &> phc2sys.log &
+#echo "phc2sys (sync NIC clock to sys clock) is running in bg as pid $!"
