ddb1d04bcb756ade22bb21c1d32ad966ac781caa
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-12-20 23:17:25 +0000

    test ptp in foreground

--- ptp/gptp/run_gptp_master.sh
+++ ptp/gptp/run_gptp_master.sh
@@ -6,11 +6,13 @@ if [[ `whoami` != "root" ]]; then
     echo "Run as root/sudo!"; exit -1;
 fi
 
+DIR=$(dirname ${BASH_SOURCE})
+echo $DIR
 echo "Start ptp..."
-sudo ptp4l -f gPTP_master.cfg -i enp88s0 -m &> ptp.log &
+sudo ptp4l -f $DIR/gPTP_master.cfg -i enp88s0 -m #&> $DIR/ptp.log &
 echo "ptp4l is running in bg as pid $!"
 
-sleep 2
+#sleep 2
 
 #echo "Start phc2sys..."
 #sudo phc2sys -c enp88s0 -s CLOCK_REALTIME -m -O 37 &> phc2sys.log &
