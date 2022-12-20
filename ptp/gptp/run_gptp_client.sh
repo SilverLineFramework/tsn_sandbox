100bc3ab8af32370ceaf26ba958106d282386518
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-12-20 23:39:03 +0000

    ptp4l test without phc

--- ptp/gptp/run_gptp_client.sh
+++ ptp/gptp/run_gptp_client.sh
@@ -9,7 +9,7 @@ fi
 DIR=$(dirname ${BASH_SOURCE})
 echo $DIR
 echo "Start ptp..."
-sudo ptp4l -f $DIR/gPTP_client.cfg -i enp88s0 -m #&> $DIR/ptp.log &
+sudo ptp4l -f $DIR/gPTP_client.cfg -i enp88s0 -m &> $DIR/ptp.log &
 echo "ptp4l is running in bg as pid $!"
 
 #sleep 2
